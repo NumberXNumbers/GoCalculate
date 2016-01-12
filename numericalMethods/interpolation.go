a74aeb7353818b6f3ada83e25bd4e605bd3c5707
TRAVIS ALLEN SALAS COX <traviscox1990@utexas.edu>
2016-01-12 17:31:31 -0600

    changes to make newton's method of interpolating
    in order to make it more effective

--- numericalMethods/interpolation.go
+++ numericalMethods/interpolation.go
@@ -1,6 +1,10 @@
-package numericalMethods
+// package numericalMethods
+
+package main
 
 import "errors"
+import "log"
+import "fmt"
 
 // NewtonDividedDifference is for calculating the coefficients for newton's divided-difference interpolating polynomial
 func NewtonDividedDifference(xValues []float64, functionValues []float64) ([]float64, error) {
@@ -10,18 +14,29 @@ func NewtonDividedDifference(xValues []float64, functionValues []float64) ([]flo
 		return nil, errors.New("Length of x values array and function values array does not match")
 	}
 
-	currentFunctionValues := make([]float64, size)
+	previousValue := float64(0)
+	tempValue := float64(0)
 
 	for i := 1; i < size; i++ {
-		// fmt.Println(solutionArray)
+		previousValue = functionValues[i-1]
 		for j := i; j < size; j++ {
-			currentFunctionValues[j] = (functionValues[j] - functionValues[j-1]) / (xValues[j] - xValues[j-i])
-		}
-
-		for j := i; j < size; j++ {
-			functionValues[j] = currentFunctionValues[j]
+			tempValue = functionValues[j]
+			functionValues[j] = (functionValues[j] - previousValue) / (xValues[j] - xValues[j-i])
+			previousValue = tempValue
 		}
 	}
 
 	return functionValues, nil
+}
+
+func main() {
+	xValues := []float64{1, 1.3, 1.6, 1.9, 2.2}
+	functionValues := []float64{0.7651977, 0.6200860, 0.4554022, 0.2818186, 0.1103623}
+	results, err := NewtonDividedDifference(xValues, functionValues)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(results)
 }
