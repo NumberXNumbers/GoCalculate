25963ed9ede5e002b5f6bcb4eddbd243a3acde56
Will Plusnick <pwp@utexas.edu>
2016-01-16 15:38:58 -0600

    Format numericalMethods/integration.go using gofmt

--- numericalMethods/integration.go
+++ numericalMethods/integration.go
@@ -2,41 +2,41 @@ package numericalMethods
 
 // Euler1D is for solving the numerical integration 1D euler method
 func Euler1D(a float64, b float64, N int, initValue float64, f func(float64, float64) float64) float64 {
-     h := (b - a) / float64(N)
-     t := a
-     omega := initValue
+	h := (b - a) / float64(N)
+	t := a
+	omega := initValue
 
-     for i := 0; i < N; i++ {
-     	 omega += h * f(t, omega)
-	       t += h
-	       }
+	for i := 0; i < N; i++ {
+		omega += h * f(t, omega)
+		t += h
+	}
 
-	       return omega
+	return omega
 }
 
 // TrapezoidRule is for solving the numerical integration using the trapezoid rule
 func TrapezoidRule(a float64, b float64, N int, f func(float64) float64) float64 {
-     var omega float64
-     h := (b - a) / float64(N)
-     x := a
+	var omega float64
+	h := (b - a) / float64(N)
+	x := a
 
-     for i := 0; i < N; i++ {
-     	 omega += f(x+h)+f(x)
-	 x += h
-     }
+	for i := 0; i < N; i++ {
+		omega += f(x+h) + f(x)
+		x += h
+	}
 
-     return h/2 * omega
+	return h / 2 * omega
 }
 
 // SimpsonRule for solving numerical integration
 func SimpsonRule(a float64, b float64, N int, f func(float64) float64) float64 {
-     var omega float64
-     h := (b - a) / float64(N)
-     x := a
+	var omega float64
+	h := (b - a) / float64(N)
+	x := a
 
-     for i := 0; i < N; i++ {
-     	 omega += f(x) + 4 * f(x+h) + f(x+2*h)
-	 x += h
-     }
-     return h/6 * omega
+	for i := 0; i < N; i++ {
+		omega += f(x) + 4*f(x+h) + f(x+2*h)
+		x += h
+	}
+	return h / 6 * omega
 }
