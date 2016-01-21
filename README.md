a5353a720b1228ecbe33691c5b31d74ae6113691
TRAVIS ALLEN SALAS COX <traviscox1990@utexas.edu>
2016-01-20 18:43:08 -0600

    linted and added to readme
    realized there were some linting issues with code that was push up after
    atom auto saved and changed something automatically. decided to fix those really quickly and add to the readme to help make sure that doesn't happen again.

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # GoCalculate
+
+## Overview
+
+**_GoCalculate_** is a library built with Google's [Go](https://golang.org/) programming language containing many Numerical Methods, Functions and Algorithms useful in math and science.
+
+## Contributing
+
+Feel free to drop a pull request to add new features that you might like to see in **_GoCalculate_**. We request that you first run [goimports](https://godoc.org/golang.org/x/tools/cmd/goimports), [golint](https://github.com/golang/lint) and [go vet](https://golang.org/cmd/vet/) on code before submitting a pull request.
+
+## License
+
+**_GoCalculate_** is licensed under the [MIT License](https://opensource.org/licenses/MIT)
--- numericalMethods/polynomial.go
+++ numericalMethods/polynomial.go
@@ -4,31 +4,30 @@ import "math"
 
 // LegendrePolynomial returns P_n(x) where P_n is the nth degree legendre polynomial
 func LegendrePolynomial(n int, x float64) float64 {
-     var sum float64 = 0
-     for k := 0; k <= n; k++ {
-     	 sum += math.Pow(BinomialCoefficient(n, k), 2.0) * math.Pow(x-1.0, float64(n-k)) * math.Pow(x+1, float64(k))
-     }
-     return math.Exp2(-float64(n))*sum
+	var sum float64
+	for k := 0; k <= n; k++ {
+		sum += math.Pow(BinomialCoefficient(n, k), 2.0) * math.Pow(x-1.0, float64(n-k)) * math.Pow(x+1, float64(k))
+	}
+	return math.Exp2(-float64(n)) * sum
 }
 
 // LegendrePolynomialNGenerator returns a function that takes x and returns P_n(x)
 func LegendrePolynomialNGenerator(n int) func(x float64) float64 {
-     return func(x float64) float64 {
-     	    	   var sum float64 = 0
-		   for k := 0; k <= n; k++ {
-		       sum += math.Pow(BinomialCoefficient(n, k), 2.0) * math.Pow(x-1.0, float64(n-k)) * math.Pow(x+1, float64(k))
-     		   }
-		   return math.Exp2(-float64(n))*sum
-     	    }
+	return func(x float64) float64 {
+		var sum float64
+		for k := 0; k <= n; k++ {
+			sum += math.Pow(BinomialCoefficient(n, k), 2.0) * math.Pow(x-1.0, float64(n-k)) * math.Pow(x+1, float64(k))
+		}
+		return math.Exp2(-float64(n)) * sum
+	}
 }
-
 
 // BinomialCoefficient returns n choose k
 func BinomialCoefficient(n int, k int) float64 {
-     var prod float64 = 1.0
-     for i := 1; i <= k; i++ {
-     	 prod *= float64(n - k + i) / float64(i)
-     }
+	prod := 1.0
+	for i := 1; i <= k; i++ {
+		prod *= float64(n-k+i) / float64(i)
+	}
 
-     return prod
+	return prod
 }
