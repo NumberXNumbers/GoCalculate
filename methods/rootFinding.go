67dd04a758b1e103b29e71f35f04dff04179c008
TRAVIS ALLEN SALAS COX <traviscox1990@utexas.edu>
2016-03-06 17:49:05 -0600

    added more tests for methods
    added tests for all root findings methods

--- methods/rootFinding.go
+++ methods/rootFinding.go
@@ -6,28 +6,28 @@ import (
 )
 
 // Bisection1D is for solving the 1D root finding bisection method
-func Bisection1D(a float64, b float64, TOL float64, maxIteration int, f func(x float64) float64) (float64, error) {
-	fOfA := f(a)
-	currentX := a + (b-a)/float64(2)
+func Bisection1D(intervalBegin float64, intervalEnd float64, TOL float64, maxIteration int, f func(x float64) float64) (float64, error) {
+	fOfA := f(intervalBegin)
+	currentX := intervalBegin + (intervalEnd-intervalBegin)/float64(2)
 	fOfCurrentX := f(currentX)
 	root := float64(0)
 	solutionFound := false
 
 	for i := 0; i < maxIteration; i++ {
-		if fOfCurrentX == 0 || (b-a)/float64(2) < TOL {
+		if fOfCurrentX == 0 || (intervalEnd-intervalBegin)/float64(2) < TOL {
 			root = currentX
 			solutionFound = true
 			break
 		}
 
 		if fOfA*fOfCurrentX > 0 {
-			a = currentX
+			intervalBegin = currentX
 			fOfA = fOfCurrentX
 		} else {
-			b = currentX
+			intervalEnd = currentX
 		}
 
-		currentX = a + (b-a)/float64(2)
+		currentX = intervalBegin + (intervalEnd-intervalBegin)/float64(2)
 		fOfCurrentX = f(currentX)
 	}
 
