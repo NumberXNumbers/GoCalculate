6c8f61f9cf7e05b36245823361a1870c58ea4d8a
TRAVIS ALLEN SALAS COX <traviscox1990@utexas.edu>
2016-03-09 00:24:23 -0600

    fix errors

--- scripts/generate_coverage_report.sh
+++ scripts/generate_coverage_report.sh
@@ -3,7 +3,7 @@ set -ev
 touch coverage.txt
 
 for d in $(go list ./...); do
-    go test -coverprofile=profile.out -covermode=atomic $d
+    go test -coverprofile=profile.out -covermode=count $d
     if [ -f profile.out ]; then
         cat profile.out >> coverage.txt
         rm profile.out
