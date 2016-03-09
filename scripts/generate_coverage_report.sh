e01287d5bf996f9b83cf2cb2aa5089e17f110dab
TRAVIS ALLEN SALAS COX <traviscox1990@utexas.edu>
2016-03-08 22:45:28 -0600

    moving to codecov

--- scripts/generate_coverage_report.sh
+++ scripts/generate_coverage_report.sh
@@ -1,19 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -ev
-cd $TRAVIS_BUILD_DIR
-touch cover.cov
-echo "mode: count" >> cover.cov
-DIRS=$(go list ./...)
-for dir in $DIRS; do
-    go test $dir -coverprofile=cover.out -covermode=count
-    if [ $? != 0 ]
-    then
-        exit 2
-    fi
-    if [ -f cover.out ]
-    then
-        sed -i '1d' cover.out
-        cat cover.out >> cover.cov
-        rm cover.out
+touch coverage.txt
+
+for d in $(go list ./...); do
+    go test -coverprofile=profile.out -covermode=atomic $d
+    if [ -f profile.out ]; then
+        cat profile.out >> coverage.txt
+        rm profile.out
     fi
 done
