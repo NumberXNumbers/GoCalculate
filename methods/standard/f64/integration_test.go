a1469dae0dcf0f25652fa1bb73fa8136bbb986f4
TRAVIS ALLEN SALAS COX <traviscox1990@utexas.edu>
2016-07-18 20:49:45 -0500

    added more tests

--- methods/standard/f64/integration_test.go
+++ methods/standard/f64/integration_test.go
@@ -69,7 +69,7 @@ func TestBooleRule(t *testing.T) {
 
 func TestRungeKutta(t *testing.T) {
 	f := func(x, y float64) float64 {
-		return y - math.Pow(float64(x), 2) + 1
+		return y - math.Pow(x, 2) + 1
 	}
 	a := 0.0
 	b := 2.0
@@ -116,6 +116,38 @@ func TestHeun(t *testing.T) {
 	initialCondition := 0.5
 	solutionMatrix := Heun(a, b, N, initialCondition, f)
 	if result := solutionMatrix[10][1]; math.Abs(result-5.3054720) > 1e-1 {
+		t.Fail()
+	}
+}
+
+func TestAdamsBashforth(t *testing.T) {
+	f := func(x, y float64) float64 {
+		return y - math.Pow(x, 2) + 1
+	}
+	a := 0.0
+	b := 2.0
+	N := 10
+	initialCondition1 := 0.5
+	initialCondition2 := 0.8292986
+	initialCondition3 := 1.2140877
+	initialCondition4 := 1.6489406
+	initialCondition5 := 2.1272295
+	solutionMatrixA := AdamsBashforth2(a, b, N, initialCondition1, initialCondition2, f)
+	if result := solutionMatrixA[10][1]; math.Abs(result-5.3054720) > 1e-1 {
+		t.Fail()
+	}
+	solutionMatrixB := AdamsBashforth3(a, b, N, initialCondition1, initialCondition2, initialCondition3, f)
+	if result := solutionMatrixB[10][1]; math.Abs(result-5.3054720) > 1e-1 {
+		t.Fail()
+	}
+	solutionMatrixC := AdamsBashforth4(a, b, N, initialCondition1, initialCondition2, initialCondition3, initialCondition4, f)
+
+	if result := solutionMatrixC[10][1]; math.Abs(result-5.3054720) > 1e-1 {
+		t.Fail()
+	}
+	solutionMatrixD := AdamsBashforth5(a, b, N, initialCondition1,
+		initialCondition2, initialCondition3, initialCondition4, initialCondition5, f)
+	if result := solutionMatrixD[10][1]; math.Abs(result-5.3054720) > 1e-1 {
 		t.Fail()
 	}
 }
